3f789b1d0ed530339f377ca7f6bd66b9630d7664
EC2 Default User <ec2-user@ip-172-31-14-166.us-east-2.compute.internal>
2018-03-04 23:11:09 +0000

    Homepage visuals nearly complete

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    @icons = ["triprequest", "calendar", "sponsors", "donate", "register", "advisory"]
+    @icondesc = ["TRIP REQUEST", "OPEN MISSIONS", "CORPORATE SPONSORS", "DONATE",
+    "REGISTER YOUR AIRCRAFT", "OUR ADVISORY BOARD"]
+    @iconpaths = [passengers_travel_path, pilots_available_missions_path,
+    corporate_recognition_path, donate_path, aircraft_registration_path, advisory_board_path]
     
   end
   def advisory_board
