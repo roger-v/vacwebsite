5421a42b7c1817147609b940cc9bae1aa2bf8933
EC2 Default User <ec2-user@ip-172-31-16-49.us-east-2.compute.internal>
2018-03-25 18:42:47 +0000

    Added login functionality, and helper methods in Application Controller for session management.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  helper_method :current_user, :logged_in? 
+  #The above line makes these methods available to the views
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] #return user if they're logged in
+    #session[:user_id] returns NIL if user is not logged in
+    
+  end
+  
+  def logged_in?
+    !!current_user #boolean conversion based on nil-ness
+  end
+  
+  def require_user
+    if (!logged_in?)
+      flash[:danger] = "You must be logged in to do that."
+      redirect_to root_path
+    end
+  end
+  
 end
