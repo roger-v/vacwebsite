f4c5e2c53ac8ad578dd741ada82008ee6e445317
EC2 Default User <ec2-user@ip-172-31-16-49.us-east-2.compute.internal>
2018-03-25 03:35:57 +0000

    Created users table, added signup (create users) functionality

--- db/migrate/20180307012227_remove_password_from_users.rb
+++
@@ -1,4 +0,0 @@
-class RemovePasswordFromUsers < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/migrate/20180325025453_create_users.rb
+++ db/migrate/20180325025453_create_users.rb
@@ -1,12 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :firstname
+      t.string :lastname
       t.string :email
-      t.string :password
-
-      t.timestamps
+      
+      t.timestamp
     end
   end
 end
