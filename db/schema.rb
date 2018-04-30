3202bfec0c92c33e8501f306470c4274ff07b115
BIGHEAD2783 <bighead2783@gmail.com>
2018-04-30 16:42:27 +0000

    Complete Corporate Recognition & Privacy Policy NO ROUTE

--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,11 @@ ActiveRecord::Schema.define(version: 20180329173856) do
     t.string "email"
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean "veteran", default: false
     t.boolean "pilot", default: false
     t.boolean "donor", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
